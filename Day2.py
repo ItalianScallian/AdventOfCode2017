803961f1a718530b06b14d6aa96a484e3fae8421
Romeo Scagliarini <romeoscagliarini@yahoo.com>
2017-12-03 00:19:41 -0600

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,20 @@
+import re
+inp = [i.strip() for i in open('input.txt', 'r').readlines()]
+
+s1 = 0
+s2 = 0
+
+for line in inp:
+    nums = [int(u) for u in re.split('\s+', line)]
+
+    s1 += max(nums) - min(nums)
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i == j: continue
+            if nums[i] % nums[j] == 0:
+                s2 += nums[i] / nums[j]
+
+
+print "Part 1:", s1
+print "Part 2:", s2
